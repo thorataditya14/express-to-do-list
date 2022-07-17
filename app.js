b26cf3893bab2978d73014e38ec17664f3006a63
Aditya Thorat <thorat.aditya@outlook.com>
2022-07-17 20:04:16 +0530

    complete express-to-do-list

--- app.js
+++ app.js
@@ -168,7 +168,8 @@ app.post("/delete", function (req, res) {
     }
 });
 
+port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(port, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
